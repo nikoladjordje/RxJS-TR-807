69cbfbf70df290e4b45c235146badcf70c35328e
nikoladjordje <nikola.djordje.mail@protonmail.com>
2023-09-11 02:05:50 +0200

    Selecting steps on sequencer

--- src/TR808.ts
+++ src/TR808.ts
@@ -1,4 +1,10 @@
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
+
+// Create an array to track the steps
+const stepStates = new Array(16).fill(false);
+
+// Create a subject to emit click events
+const stepClickSubject = new Subject<number>();
 
 export function drawSequencer() {
   // Create a 1x16 grid of drum pads
@@ -13,7 +19,7 @@ export function drawSequencer() {
 
     const step = document.createElement("div");
     step.classList.add("step");
-    step.classList.add((i + 1).toString());
+    step.classList.add("step-" + (i + 1).toString());
     if (i >= 0 && i < 4) step.classList.add("first");
     if (i >= 4 && i < 8) step.classList.add("second");
     if (i >= 8 && i < 12) step.classList.add("third");
@@ -26,5 +32,26 @@ export function drawSequencer() {
     pad.appendChild(num);
 
     seqContainer.appendChild(pad);
+    // Add click event to the step
+    pad.addEventListener("click", () => {
+      stepStates[i] = !stepStates[i];
+      if (stepStates[i])
+        document
+          .querySelector(".step-" + (i + 1).toString())
+          .classList.add("step-selected");
+      else
+        document
+          .querySelector(".step-" + (i + 1).toString())
+          .classList.remove("step-selected");
+      stepClickSubject.next(i);
+    });
   }
 }
+
+// Create an observable to listen for step click events
+export const stepClickObservable = stepClickSubject.asObservable();
+
+stepClickObservable.subscribe((clickedStep) => {
+  console.log(`Step ${clickedStep + 1} was clicked.`);
+  console.log(stepStates);
+});
