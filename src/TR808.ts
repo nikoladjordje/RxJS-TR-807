2eae0b76c70f5abbdc41fdf44965eca6584e4a72
nikoladjordje <nikola.djordje.mail@protonmail.com>
2023-09-12 23:14:36 +0200

    Functional Sequencer

--- src/TR808.ts
+++ src/TR808.ts
@@ -1,4 +1,14 @@
-import { Observable, Subject } from "rxjs";
+import {
+  Observable,
+  Subject,
+  fromEvent,
+  interval,
+  map,
+  scan,
+  startWith,
+  switchMap,
+  takeUntil,
+} from "rxjs";
 
 // Create an array to track the steps
 const stepStates = new Array(16).fill(false);
@@ -7,10 +17,88 @@ const stepStates = new Array(16).fill(false);
 const stepClickSubject = new Subject<number>();
 
 export function drawSequencer() {
+  // Create Start button
+  const startButton = document.createElement("button");
+  startButton.classList.add("start-btn");
+  startButton.innerHTML = "Start";
+  document.querySelector(".main").appendChild(startButton);
+
+  // Create Stop button
+  const stopButton = document.createElement("button");
+  stopButton.classList.add("stop-btn");
+  stopButton.innerHTML = "Stop";
+  document.querySelector(".main").appendChild(stopButton);
+
+  // Create observables for button clicks
+  const startClick$ = fromEvent(startButton, "click");
+  const stopClick$ = fromEvent(stopButton, "click");
+
+  // Create an interval observable
+  const interval$ = interval(100);
+
+  // Create an observable to start and stop the interval
+  const intervalControl$ = startClick$.pipe(
+    switchMap(() =>
+      interval$.pipe(
+        startWith(-1),
+        scan((acc) => (acc + 1) % 16, -1),
+        takeUntil(stopClick$)
+      )
+    )
+  );
+
+  // Subscribe to the intervalControl$ observable and update the output
+  intervalControl$.subscribe(
+    (value) => {
+      console.log(`Interval value: ${value}`);
+      if (stepStates[value]) playSoundById(6);
+    },
+    null,
+    () => {
+      console.log("Interval stopped.");
+    }
+  );
+
+  // Define a function to create an audio element for each sound
+  function createAudioElement(soundPath: string): HTMLAudioElement {
+    const audio = new Audio(soundPath);
+    audio.preload = "auto";
+    return audio;
+  }
   // Create a 1x16 grid of drum pads
   const seqContainer = document.createElement("div");
   seqContainer.classList.add("seq-container");
   document.querySelector(".main").appendChild(seqContainer);
+
+  // Create an array to store the audio elements for each sound
+  const audioElements: { id: number; audio: HTMLAudioElement }[] = [];
+
+  // Create a function to play a sound by ID
+  function playSoundById(id: number) {
+    const audio = audioElements.find((element) => element.id === id)?.audio;
+    if (audio) {
+      audio.currentTime = 0;
+      audio.play();
+    }
+  }
+
+  // Fetch data from JSON Server API
+  const apiUrl = "http://localhost:3000/sounds";
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Populate the audioElements array with audio elements
+      audioElements.push(
+        ...data.map((sound: any) => ({
+          id: sound.id,
+          audio: createAudioElement(`/assets/sounds/${sound.soundPath}`), // Update the URL
+        }))
+      );
+    })
+    .catch((error) => {
+      console.error("Error fetching data from the server:", error);
+    });
+  console.log(audioElements);
 
   // Create the sequencer steps
   for (let i = 0; i < 16; i++) {
